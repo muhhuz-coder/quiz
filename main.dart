83a02a72e22c7a8039fe8fca71b968f705afef04
Muhammad Huzaifa <110600384+muhhuz-coder@users.noreply.github.com>
2023-04-15 17:25:50 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,19 @@
+void main(){
+  
+   String number = "11222333377777";
+   int i=0,count=1;
+   String numoutput='';
+for(;i<number.length;){
+   while(number[i+1]==number[i]){
+       count++;
+       i++;
+  };
+numoutput= numoutput +'$count';
+numoutput= numoutput +"${number[i]}";
+ // i=i+1;
 
+  count=1;
+}
+  print(numoutput);
+    
+}
